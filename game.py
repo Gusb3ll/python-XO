8b6328debd87ab23e10d4009561538ee002dbed7
winth03 <66255462+winth03@users.noreply.github.com>
2022-09-30 19:50:44 +0700

    draw grid func

--- game.py
+++ game.py
@@ -1,4 +1,28 @@
 import turtle as t
 
-t.screensize(750, 750, "white")
+
+def DrawScreen():
+    # Initialize screen settings
+    t.screensize(750, 750, "gray")
+    t.setworldcoordinates(0, 750, 750, 0)
+    t.pensize(10)
+
+    # Draw grid lines
+    t.color("white")
+    t.pu()
+    
+    for y in range(1, 3):
+        t.setpos(0, y * 250)
+        t.pendown()
+        t.goto(750, y * 250)
+        t.penup()
+    for x in range(1, 3):
+        t.setpos(x * 250, 0)
+        t.pendown()
+        t.goto(x * 250, 750)
+        t.penup()
+
+
+
+DrawScreen()
 input()
