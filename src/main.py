730b0ab4781e7fc15ad2e6f2dbe4acac6b515bc5
winth03 <66255462+winth03@users.noreply.github.com>
2022-10-31 17:27:05 +0700

    feat: hint implemented

--- src/main.py
+++ src/main.py
@@ -7,19 +7,29 @@ class Model:
         self.screen = turtle.Screen()
         self.gt = turtle.Turtle() # To draw grid lines
         self.st = turtle.Turtle() # To draw symbols
+        self.ht = turtle.Turtle() # To draw hints
         self.board = [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
         ]
-        self.player = 'O'
-        self.winner = ''
+        self.hint = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        self.player = 1
+        self.winner = 0
 
     def change_player(self):
-        if self.player == 'O':
-            self.player = 'X'
-        else:
-            self.player = 'O'
+        self.player = -self.player
+
+    def reset_hint(self):
+        self.hint = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
 
     def load_save(self):
         with open('save.json', 'r') as saveFile:
@@ -47,6 +57,7 @@ class View:
         self.model.screen.bgcolor("#346B31")
         self.model.gt.pen(shown=False, pendown=False, pencolor="white", pensize=10, speed=0)
         self.model.st.pen(shown=False, pendown=False, pencolor="white", pensize=10, resizemode="user", stretchfactor=(2.5, 2.5), speed=10)
+        self.model.ht.pen(shown=False, pendown=False, pencolor="red", pensize=10, resizemode="user", stretchfactor=(2.5, 2.5), speed=0)
         self.model.st.setheading(90)
         self.model.screen.tracer(False)
 
@@ -67,19 +78,19 @@ class View:
 
         self.model.screen.tracer(True)
 
-    def draw_loaded_symbol(self):
+    def draw_all_symbols(self): # draw all symbols without animation
         self.model.screen.tracer(False)
         for y in range(3):
             for x in range(3):
                 self.draw_new_symbol(self.model.board[y][x], x, y)
         self.model.screen.tracer(True)
 
-    def draw_new_symbol(self, shape, x, y):
-        if shape == 'O':
+    def draw_new_symbol(self, shape, x, y): # draw new symbol with animation
+        if shape == 1:
             self.model.st.setpos(x * 250 + 225, y * 250 + 125)
             self.model.st.pendown()
             self.model.st.circle(100, steps=20)
-        elif shape == 'X':
+        elif shape == -1:
             self.model.st.setpos(x * 250 + 25, y * 250 + 25)
             self.model.st.pendown()
             self.model.st.goto(x * 250 + 225, y * 250 + 225)
@@ -93,7 +104,7 @@ class View:
 
     def show_winner(self, x1, y1, x2, y2, resetFunc):
         FONT_SIZE = 72
-        if not (x1==y1==x2==y2==0):
+        if not (x1==y1==x2==y2==0): # if not a tie draw red line
             self.model.st.pencolor("red")
             self.model.st.goto(x1, y1)
             self.model.st.pendown()
@@ -101,10 +112,21 @@ class View:
             self.model.st.penup()
         self.model.st.goto(self.model.screen.window_width()/2, self.model.screen.window_height()/2 + FONT_SIZE/2)
         self.model.st.pencolor("black")
-        self.model.st.write(f"{self.model.winner} WINS" if self.model.winner != "tie" else "TIE", align="center", font=("Arial", FONT_SIZE, "bold"))
+        self.model.st.write(f"{ 'O' if self.model.winner == 1 else 'X' } WINS" if self.model.winner != 2 else "TIE", align="center", font=("Comic Sans MS", FONT_SIZE, "bold")) # -1 = X wins, 1 = O wins, 2 = tie
         self.model.st.pencolor("white")
 
         self.model.screen.onclick(resetFunc)
+
+    def show_hint(self):
+        FONT_SIZE = 72
+        self.model.screen.tracer(False)
+        self.model.ht.clear()
+        for y in range(3):
+            for x in range(3):
+                if self.model.hint[y][x] == 1:
+                    self.model.ht.setpos(x * 250 + 125, y * 250 + 125 + FONT_SIZE)
+                    self.model.ht.write("?", align="center", font=("Comic Sans MS", FONT_SIZE, "bold"))
+        self.model.screen.tracer(True)
 
 
 class Controller:
@@ -118,9 +140,10 @@ class Controller:
             model.update_save()
         
         view.draw_screen()
-        view.draw_loaded_symbol()
+        view.draw_all_symbols()
         model.screen.onclick(self.update_board)
         self.check_win_tie()
+        self.check_hint()
         model.screen.listen()
         model.screen.mainloop()
 
@@ -129,7 +152,7 @@ class Controller:
         x = sorted((0, int(x // 250), 2))[1]
         y = sorted((0, int(y // 250), 2))[1]
 
-        if self.model.st.isdown() or self.model.board[y][x] != '':
+        if self.model.st.isdown() or self.model.board[y][x] != 0:
             return
         self.model.screen.onclick(None)
         self.model.board[y][x] = self.model.player
@@ -139,48 +162,69 @@ class Controller:
         self.model.update_save()
         self.model.screen.onclick(self.update_board)
         self.check_win_tie()
+        self.check_hint()
 
     def check_win_tie(self):
         tie = True
         offset_width = self.model.screen.window_width() - self.model.st.width()
         for i in range(3):
             # Horizontal
-            if self.model.board[i][0] == self.model.board[i][1] == self.model.board[i][2] != '':
+            if abs(sum(self.model.board[i])) == 3:
                 self.model.winner = self.model.board[i][0]
                 self.view.show_winner(0, i*250+125, offset_width, i*250+125, self.reset_board)
                 return
             # Vertical
-            if self.model.board[0][i] == self.model.board[1][i] == self.model.board[2][i] != '':
+            if abs(sum(self.model.board[j][i] for j in range(3))) == 3:
                 self.model.winner = self.model.board[0][i]
                 self.view.show_winner(i*250+125, 0, i*250+125, offset_width, self.reset_board)
                 return
             # Tie
-            if '' in self.model.board[i]:
+            if 0 in self.model.board[i]:
                 tie = False
         # Diagonal (Left-Right)
-        if self.model.board[0][0] == self.model.board[1][1] == self.model.board[2][2] != '':
+        if abs(sum(self.model.board[i][i] for i in range(3))) == 3:
             self.model.winner = self.model.board[0][0]
             self.view.show_winner(0, 0, offset_width, offset_width, self.reset_board)
             return
         # Diagonal (Right-Left)
-        if self.model.board[0][2] == self.model.board[1][1] == self.model.board[2][0] != '':
+        if abs(sum(self.model.board[i][2-i] for i in range(3))) == 3:
             self.model.winner = self.model.board[0][2]
             self.view.show_winner(offset_width, 0, 0, offset_width, self.reset_board)
             return
         if tie:
-            self.model.winner = "tie"
+            self.model.winner = 2
             self.view.show_winner(0, 0, 0, 0, self.reset_board)
             return
 
+    def check_hint(self):
+        self.model.reset_hint()
+        for i in range(3):
+            # Horizontal
+            if abs(sum(self.model.board[i])) == 2:
+                self.model.hint[i][self.model.board[i].index(0)] = 1
+            # Vertical
+            vertical = [self.model.board[j][i] for j in range(3)]
+            if abs(sum(vertical)) == 2:
+                self.model.hint[vertical.index(0)][i] = 1
+        # Diagonal (Left-Right)
+        dia1 = [self.model.board[i][i] for i in range(3)]
+        if abs(sum(dia1)) == 2:
+            self.model.hint[dia1.index(0)][dia1.index(0)] = 1
+        # Diagonal (Right-Left)
+        dia2 = [self.model.board[i][2-i] for i in range(3)]
+        if abs(sum(dia2)) == 2:
+            self.model.hint[dia2.index(0)][2-dia2.index(0)] = 1
+        self.view.show_hint()
+
     def reset_board(self, x, y):
         self.model.board = [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
         ]
         self.model.st.clear()
-        self.model.winner = ''
-        self.model.player = 'O'
+        self.model.winner = 0
+        self.model.player = 1
         self.model.update_save()
         self.model.screen.onclick(self.update_board)
 
